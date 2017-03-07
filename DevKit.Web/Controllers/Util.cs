f56e1b676131c6816071519ebb7a26b8c01ebd07
rodrigogroff <rodrigo.groff@CIT77>
2017-03-07 12:01:45 -0300

    fix

--- DevKit.Web/Controllers/Util.cs
+++ DevKit.Web/Controllers/Util.cs
@@ -12,7 +12,7 @@ namespace DevKit.Web.Controllers
 		public User GetCurrentUser(DevKitDB db)
 		{
 			return (from ne in db.Users
-					where ne.stLogin.ToUpper() == Thread.CurrentPrincipal.Identity.Name
+					where ne.stLogin.ToUpper() == Thread.CurrentPrincipal.Identity.Name.ToUpper()
 					select ne).FirstOrDefault();
 		}
 	}

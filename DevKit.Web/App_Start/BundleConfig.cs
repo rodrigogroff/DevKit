43c59f5ae8eff19d4e89c832f54f776bae0c6039
rodrigogroff <rodrigo.groff@CIT77>
2017-03-21 16:38:11 -0300

    fix

--- DevKit.Web/App_Start/BundleConfig.cs
+++ DevKit.Web/App_Start/BundleConfig.cs
@@ -7,7 +7,7 @@
 namespace DevKit.Web
 {
 	using System.Web.Optimization;
-
+	 
 	public class BundleConfig
 	{
 		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862

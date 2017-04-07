a61c57481520bf21a76fb2e1cfead3fc2d7bb09a
rodrigogroff <rodrigo.groff@CIT77>
2017-04-07 09:49:35 -0300

    fix

--- DevKit.Web/app/login/loginController.js
+++ DevKit.Web/app/login/loginController.js
@@ -3,40 +3,44 @@ angular.module('app.controllers').controller('LoginController',
 ['$scope', '$rootScope', '$location', 'AuthService', 'version',
 function ($scope, $rootScope, $location, AuthService, version)
 {
-	$scope.version = version;
 	$rootScope.exibirMenu = false;
+
+	$scope.version = version;	
 	$scope.loading = false;
+	$scope.loginOK = false;
+	$scope.mensagem = "";
 
-    $scope.loginData = {
-        userName: "",
-        password: ""
-    };
-
-    $scope.mensagem = "";
+	$scope.loginData =
+		{
+			userName: "",
+			password: ""
+		};
 
     $scope.login = function ()
     {
     	$scope.loading = true;
 
-    	if ($scope.loginData.userName == '' || $scope.loginData.password == '')
+    	if ($scope.loginData.userName == '' ||
+			$scope.loginData.password == '')
     	{
     		$scope.loading = false;
     		$scope.mensagem = 'Please enter valid credentials';
     	}
     	else
     	{
-    		AuthService.login($scope.loginData).then(function (response) {
-    			$scope.loading = false;
-
+    		AuthService.login($scope.loginData).then(function (response)
+    		{
+    			$scope.loginOK = true;
     			$rootScope.exibirMenu = true;
     			$rootScope.$broadcast('updateCounters');
-
     			$location.path('/');
     		},
-			function (err) {
+			function (err)
+			{
 				$scope.loading = false;
 				$scope.mensagem = err.error_description;
 			});
     	}
     };
+
 }]);

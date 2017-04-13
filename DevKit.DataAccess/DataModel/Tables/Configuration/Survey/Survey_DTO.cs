fb5be273add1d516e9dc04e71e9f7feac1a2bc8f
rodrigogroff <rodrigo.groff@CIT77>
2017-04-13 14:59:18 -0300

    fix

--- DevKit.DataAccess/DataModel/Tables/Configuration/Survey/Survey_DTO.cs
+++ DevKit.DataAccess/DataModel/Tables/Configuration/Survey/Survey_DTO.cs
@@ -4,14 +4,23 @@ namespace DataModel
 {
 	public partial class Survey
 	{
+		public bool bUserSelected;
+		
 		public object anexedEntity;
 
 		public string sfkUser = "",
 						sfkProject = "",
 						sdtLog = "",
+						sNuPct = "",				
 						updateCommand = "";
 
 		public List<SurveyOption> options;
+	}
+
+	public partial class SurveyOption
+	{
+		public bool bChecked;
+		public string sNuOptionPct;
 	}
 
 	public class SurveySelOption
--- DevKit.DataAccess/DataModel/Views/Home_view.cs
+++ DevKit.DataAccess/DataModel/Views/Home_view.cs
@@ -44,17 +44,53 @@ namespace DataModel
 			{
 				var surveys = from e in db.Surveys
 							  where e.fkProject == null || projects.Contains(e.fkProject)
+							  where e.bActive == true
 							  select e;
 
-				var surveysMark = (from e in db.SurveyUserOptions where e.fkUser == user.id select e.fkSurvey).ToList();
-
-				if (surveysMark.Count() > 0)
-					surveys = from e in surveys where !surveysMark.Contains(e.id) select e;
+				var surveysMark = (from e in db.SurveyUserOptions
+								   where e.fkUser == user.id
+								   select e).
+								   ToList();
 
 				dto.surveys = surveys.OrderByDescending(y => y.id).ToList();
 
-				foreach (var item in dto.surveys)
-					item.LoadAssociations(db);
+				var nuGlobalUsers = (from e in db.Users where e.bActive == true select e).Count();
+
+				foreach (var survey in dto.surveys)
+				{
+					survey.LoadAssociations(db);
+
+					var nuSelUsers = (from e in db.SurveyUserOptions
+											where e.fkSurvey == survey.id
+											select e).
+											Count();
+
+					if (survey.fkProject == null)
+					{
+						survey.sNuPct = (nuSelUsers * 100 / nuGlobalUsers).ToString();
+					}
+					else
+					{
+						var nuProjectUsers = (from e in db.ProjectUsers where e.fkProject == survey.fkProject select e).Count();
+
+						survey.sNuPct = (nuSelUsers * 100 / nuProjectUsers).ToString();
+					}					
+
+					foreach (var option in survey.options)
+					{
+						option.bChecked = surveysMark.Any(y => y.fkSurvey == survey.id && y.fkUser == user.id && y.fkSurveyOption == option.id);
+
+						if (option.bChecked)
+							survey.bUserSelected = true;
+
+						var nuSelThisOption = surveysMark.Count(y => y.fkSurvey == survey.id && y.fkSurveyOption == option.id);
+
+						if (nuSelThisOption > 0)
+							option.sNuOptionPct = (nuSelUsers * 100 / nuSelThisOption).ToString();
+						else
+							option.sNuOptionPct = "0";
+					}
+				}					
 			}
 			#endregion
 

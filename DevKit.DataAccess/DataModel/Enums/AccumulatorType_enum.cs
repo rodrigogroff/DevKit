775478c22050cc3b4cd1502b2cd241e5a271858b
rodrigogroff <rodrigo.groff@CIT77>
2017-05-18 17:13:32 -0300

    fix

--- DevKit.DataAccess/DataModel/Enums/AccumulatorType_enum.cs
+++ DevKit.DataAccess/DataModel/Enums/AccumulatorType_enum.cs
@@ -13,14 +13,16 @@ namespace DataModel
 	{
 		public List<AccumulatorType> lst = new List<AccumulatorType>();
 
-		public const long	//Money = 1,
-							Hours = 2;
-
-		public EnumAccumulatorType()
+        public const long Money = 1,
+                            Hours = 2,
+                            Quantity = 3;
+        
+        public EnumAccumulatorType()
 		{
-			//lst.Add(new AccumulatorType() { id = 1, stName = "Money" });
+			lst.Add(new AccumulatorType() { id = 1, stName = "Money" });
 			lst.Add(new AccumulatorType() { id = 2, stName = "Hours" });
-		}
+            lst.Add(new AccumulatorType() { id = 3, stName = "Quantity" });
+        }
 
 		public AccumulatorType Get(long? _id)
 		{

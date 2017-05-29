ba71388042479ec70e6b706b08711b6283f09384
rodrigo groff <rodrigo.groff@gmail.com>
2017-05-29 18:24:07 -0300

    fix

--- DevKit.DataAccess/DataModel/Tables/System/User/User_Loader.cs
+++ DevKit.DataAccess/DataModel/Tables/System/User/User_Loader.cs
@@ -6,19 +6,16 @@ namespace DataModel
 {
 	public partial class User
 	{
-		public List<User> Loader(DevKitDB db, List<User> results, bool precached)
+		public List<User> Loader(DevKitDB db, List<User> results)
         {
-            if (precached)
-            {
-                var lstIdsProfile = results.Select(y => y.fkProfile).Distinct().ToList();
+            var lstIdsProfile = results.Select(y => y.fkProfile).Distinct().ToList();
                 
-                if (lstIdsProfile.Any())
-                {
-                    var lst = (from e in db.Profile where lstIdsProfile.Contains(e.id) select e).ToList();
-                    foreach (var item in lst) db.Cache["Profile" + item.id] = item;
-                }
+            if (lstIdsProfile.Any())
+            {
+                var lst = (from e in db.Profile where lstIdsProfile.Contains(e.id) select e).ToList();
+                foreach (var item in lst) db.Cache["Profile" + item.id] = item;
             }
-            
+                        
             results.ForEach(y => { y = y.LoadAssociations(db); });
 
             return results;

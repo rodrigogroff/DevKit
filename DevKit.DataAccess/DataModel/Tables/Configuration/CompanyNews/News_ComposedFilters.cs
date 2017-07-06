20d657b12d288aa3edf8bc3c7b42ce418c3b7469
rodrigo groff <rodrigo.groff@gmail.com>
2017-07-05 23:20:16 -0300

    fix

--- DevKit.DataAccess/DataModel/Tables/Configuration/CompanyNews/News_ComposedFilters.cs
+++ DevKit.DataAccess/DataModel/Tables/Configuration/CompanyNews/News_ComposedFilters.cs
@@ -7,20 +7,7 @@ namespace DataModel
 	public class CompanyNewsFilter : BaseFilter
     {
 		public long? fkProject;
-
-		public string ExportString()
-		{
-			var ret = "";
-
-			ret += "Skip: " + skip + " ";
-			ret += "take: " + take + " ";
-
-			if (fkProject != null)
-				ret += "fkProject: " + fkProject + " ";
-
-			return ret;
-		}
-
+		
         public string Parameters()
         {
             return Export();
@@ -48,8 +35,7 @@ namespace DataModel
 		public CompanyNewsReport ComposedFilters(DevKitDB db, CompanyNewsFilter filter)
 		{
 			var user = db.currentUser;
-			var lstUserProjects = db.GetCurrentUserProjects();
-
+            
 			var query = from e in db.CompanyNews select e;
 
             if (!string.IsNullOrEmpty(filter.busca))
@@ -57,16 +43,11 @@ namespace DataModel
 						where e.stTitle.ToUpper().Contains(filter.busca) ||
 							  e.stMessage.ToUpper().Contains(filter.busca)
 						select e;
-
-			if (lstUserProjects.Count() > 0)
-				query = from e in query where lstUserProjects.Contains(e.id) select e;
-
+            
 			if (filter.fkProject != null)
 			{
 				query = from e in query
-						join eUser in db.ProjectUser on e.id equals eUser.fkProject
-						where e.id == eUser.fkProject
-						where eUser.fkUser == filter.fkProject
+						where e.fkProject == filter.fkProject
 						select e;
 			}
 
--- DevKit.Web/app/configuration/news/ListingNewsController.js
+++ DevKit.Web/app/configuration/news/ListingNewsController.js
@@ -7,7 +7,7 @@ function ($scope, $rootScope, AuthService, $state, ngHistoricoFiltro, Api, ngSel
 	$scope.loading = false;
 	$scope.campos = {
 		selects: {
-			projec: ngSelects.obterConfiguracao(Api.ProjectCombo, { }),
+			project: ngSelects.obterConfiguracao(Api.ProjectCombo, { }),
 		}
 	};
 	$scope.itensporpagina = 15;

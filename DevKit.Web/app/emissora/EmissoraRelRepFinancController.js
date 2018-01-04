ef3ba03d37b7eaa443b9c82865e151ecd1eb8df7
rodrigo groff <rodrigo.groff@gmail.com>
2018-01-04 13:35:13 -0200

    fix

--- DevKit.Web/app/emissora/EmissoraRelRepFinancController.js
+++ DevKit.Web/app/emissora/EmissoraRelRepFinancController.js
@@ -8,10 +8,8 @@ function ($scope, $rootScope, AuthService, $state, ngHistoricoFiltro, Api, ngSel
 
     $scope.pesquisa =
         {
-            tipo: 2,    
-            tipoFut: 1,    
-            tipoSel: 2,
-            tipoFutSel: 1,    
+            tipo: 2,                
+            tipoSel: 2,            
         };
 
     $scope.date = new Date();
@@ -28,10 +26,6 @@ function ($scope, $rootScope, AuthService, $state, ngHistoricoFiltro, Api, ngSel
         $scope.list = undefined;
     });
 
-    $scope.$watch("pesquisa.tipoFut", function (novo, antigo) {
-        $scope.list = undefined;
-    });
-    
     var invalidCheck = function (element) {
         if (element == undefined)
             return true;
@@ -44,139 +38,25 @@ function ($scope, $rootScope, AuthService, $state, ngHistoricoFiltro, Api, ngSel
     
     $scope.search = function ()
     {
-        $scope.mat_fail = invalidCheck($scope.campos.matricula);
-        $scope.mes_fail = invalidCheck($scope.campos.mes_inicial);
-        $scope.ano_fail = invalidCheck($scope.campos.ano_inicial);
-
-        if ($scope.mat_fail) 
-            return;
-
         if ($scope.pesquisa.tipo == 1)
         {
-            if (!$scope.mes_fail && !$scope.ano_fail)
-            {
-                $scope.loading = true;
-
-                var opcoes = {
-                    tipo: $scope.pesquisa.tipo,
-                    mat: $scope.campos.matricula,
-                    mes: $scope.campos.mes_inicial,
-                    ano: $scope.campos.ano_inicial
-                };
-
-                Api.EmissoraRelExtratos.listPage(opcoes, function (data) {
-                    $scope.list = data.results;
-                    $scope.associado = data.associado;
-                    $scope.cartao = data.cartao;
-                    $scope.cpf = data.cpf;
-                    $scope.total = data.total;
-                    $scope.dtEmissao = data.dtEmissao;                    
-                    $scope.pesquisa.tipoSel = 1;
-                    $scope.mesSel = $scope.campos.mes_inicial;
-                    $scope.anoSel = $scope.campos.ano_inicial;
-                    $scope.loading = false;
-                },
-                function (response) {
-                    $scope.loading = false;
-                    $scope.list = [];
-                });
-            }
-        }
-        else if ($scope.pesquisa.tipo == 2)
-        {            
             $scope.loading = true;
 
             var opcoes = {
                 tipo: $scope.pesquisa.tipo,
-                mat: $scope.campos.matricula                
             };
 
-            Api.EmissoraRelExtratos.listPage(opcoes, function (data) {
+            Api.EmissoraRelRepFinanc.listPage(opcoes, function (data) {
                 $scope.list = data.results;
-                $scope.associado = data.associado;
-                $scope.cartao = data.cartao;
-                $scope.cpf = data.cpf;
-                $scope.mesAtual = data.mesAtual;
-                $scope.total = data.total;
-                $scope.saldo = data.saldo;
-                $scope.dtEmissao = data.dtEmissao;                
-                $scope.pesquisa.tipoSel = 2;
+                $scope.dtEmissao = data.dtEmissao;                    
+                $scope.pesquisa.tipoSel = 1;
                 $scope.loading = false;
             },
             function (response) {
-                
-                toastr.error('Nenhum registro', 'Erro');
-                
                 $scope.loading = false;
                 $scope.list = [];
             });
         }
-        else if ($scope.pesquisa.tipo == 3)
-        {
-            if ($scope.pesquisa.tipoFut != undefined)
-            {            
-                if ($scope.pesquisa.tipoFut == 1)
-                {            
-                    $scope.loading = true;
-
-                    var opcoes = {
-                        tipo: $scope.pesquisa.tipo,
-                        tipoFut: $scope.pesquisa.tipoFut,
-                        mat: $scope.campos.matricula
-                    };
-
-                    Api.EmissoraRelExtratos.listPage(opcoes, function (data) {
-                        $scope.list = data.results;
-                        $scope.associado = data.associado;
-                        $scope.cartao = data.cartao;
-                        $scope.cpf = data.cpf;
-                        $scope.dtEmissao = data.dtEmissao;                        
-                        $scope.pesquisa.tipoSel = 3;
-                        $scope.pesquisa.tipoFutSel = 1;
-                        $scope.loading = false;
-                    },
-                        function (response) {
-                            toastr.error('Nenhum registro', 'Erro');
-                        $scope.loading = false;
-                        $scope.list = [];
-                    });
-                }
-                else if ($scope.pesquisa.tipoFut == 2)
-                {
-                    if (!$scope.mes_fail && !$scope.ano_fail)
-                    {
-                        $scope.loading = true;
-
-                        var opcoes = {
-                            tipo: $scope.pesquisa.tipo,
-                            tipoFut: $scope.pesquisa.tipoFut,
-                            mat: $scope.campos.matricula,
-                            mes: $scope.campos.mes_inicial,
-                            ano: $scope.campos.ano_inicial,
-                        };
-
-                        Api.EmissoraRelExtratos.listPage(opcoes, function (data) {
-                            $scope.list = data.results;
-                            $scope.total = data.total;
-                            $scope.associado = data.associado;
-                            $scope.cartao = data.cartao;
-                            $scope.cpf = data.cpf;
-                            $scope.dtEmissao = data.dtEmissao;                            
-                            $scope.mesSel = $scope.campos.mes_inicial;
-                            $scope.anoSel = $scope.campos.ano_inicial;
-                            $scope.pesquisa.tipoSel = 3;
-                            $scope.pesquisa.tipoFutSel = 2;    
-                            $scope.loading = false;
-                        },
-                            function (response) {
-                                toastr.error('Nenhum registro', 'Erro');
-                            $scope.loading = false;
-                            $scope.list = [];
-                        });
-                    }
-                }                            
-            }
-        }   
     }
 
 }]);
---
+++ DevKit.Web/proc/Domains/Entidades/Emissora/EmissoraRelRepFinanc.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Web.Http;
+using SyCrafEngine;
+using LinqToDB;
+using System;
+using System.Collections.Generic;
+
+namespace DevKit.Web.Controllers
+{
+    public class EmissoraRelRepFinancController : ApiControllerBase
+    {
+        public IHttpActionResult Get()
+        {
+            var tipo = Request.GetQueryStringValue("tipo");            
+
+            var meses = ",Janeiro,Fevereiro,Março,Abril,Maio,Junho,Julho,Agosto,Setembro,Outubro,Novembro,Dezembro".Split(',');
+
+            if (!StartDatabaseAndAuthorize())
+                return BadRequest();
+
+            var mon = new money();
+
+            switch (tipo)
+            {
+                // ------------------------
+                // movimento resumido
+                // ------------------------
+
+                case "1": 
+                    {
+                        return Ok(new
+                        {
+                            //count = lst.Count(),
+                            //results = lst,
+                            dtEmissao = DateTime.Now.ToString("dd/MM/yyyy HH:mm")
+                        });
+                    }
+            }
+
+            return BadRequest();
+        }
+    }
+}

5890efd2f7ac7edd7721801a7ea8f2a9d4ffec7e
rodrigo groff <rodrigo.groff@gmail.com>
2018-01-19 22:22:28 -0200

    fixc

--- DevKit.Web/Global.asax.cs
+++ DevKit.Web/Global.asax.cs
@@ -1,8 +1,13 @@
-﻿using System.Web;
+﻿using System.Linq;
+using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Optimization;
+using System.Threading;
+using DataModel;
+using LinqToDB;
+using System;
 
 namespace DevKit.Web
 {
@@ -24,6 +29,25 @@ namespace DevKit.Web
 				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional  },
 				namespaces: new[] { "DevKit.Web.Controllers" }
 			);
+            
+           // while (true)
+            {
+                using (var db = new AutorizadorCNDB())
+                {
+                    var dt = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day );
+                    var dt_fim = dt.AddDays(1);
+                    
+                    foreach (var log in db.LOG_Transacoes.
+                                        Where(y => y.dt_transacao > dt && y.dt_transacao < dt_fim && y.tg_confirmada.ToString() == "0").
+                                        ToList())
+                    {
+                        log.tg_confirmada = '1';
+                        db.Update(log);
+                    }
+
+                    //Thread.Sleep(60000);
+                }
+            }
         }
 	}
 }

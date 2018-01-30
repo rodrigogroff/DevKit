2aa834ea010ab899d016cb7f2686448618b3b1ba
rodrigo groff <rodrigo.groff@gmail.com>
2018-01-29 22:40:36 -0200

    fix

---
+++ DevKit.DataAccess/DataModel/Enums/Tipos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevKit.DataAccess
+{
+    public class TipoSituacao
+    {
+        public const int Habilitado = 0,
+                         Bloqueado = 1;
+    }
+
+    public class TipoExpedicao
+    {
+        public const int Requerido = 0,
+                         EmProducao = 1,
+                         Entregue = 2;
+    }
+
+    public class TipoMedico
+    {
+        public const int Medico = 0,
+                         Clinica = 1;
+    }
+
+    public class TipoSituacaoConsulta
+    {
+        public const int Solicitada = 1,
+                         Confirmada = 2,
+                         CanceladaEmissor = 3,
+                         CanceladaMedico = 4,
+                         Executada = 5;
+    }
+}

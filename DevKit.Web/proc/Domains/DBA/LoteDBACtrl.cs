60e3a536f04920f35c90175a03442d180cfa02d3
rodrigo groff <rodrigo.groff@gmail.com>
2018-11-12 09:54:24 -0200

    fix

--- DevKit.Web/proc/Domains/DBA/LoteDBACtrl.cs
+++ DevKit.Web/proc/Domains/DBA/LoteDBACtrl.cs
@@ -1,10 +1,10 @@
 ﻿using LinqToDB;
+using DataModel;
+using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.Web.Http;
 using System.Net;
-using DataModel;
-using System;
 
 namespace DevKit.Web.Controllers
 {
@@ -27,6 +27,33 @@ namespace DevKit.Web.Controllers
 
             foreach (var item in query.Skip(skip).Take(take).ToList())
                 lst.Add(item);
+
+            foreach (var item in lst)
+            {
+                switch (item.tg_sitLote)
+                {
+                    case 1: item.situacao = "Em Aberto"; break;
+                    case 2: item.situacao = "Produção na gráfica"; break;
+                    case 3: item.situacao = "Cartões ativados"; break;
+                }
+
+                item.data = Convert.ToDateTime(item.dt_abertura).ToString("dd/MM/yyyy HH:mm");
+                item.cartoes = item.nu_cartoes.ToString();
+                item.empresas = "";
+
+                var lstDetalhes = db.T_LoteCartaoDetalhe.Where(y => y.fk_lote == item.i_unique).ToList();
+
+                var lstEmps = lstDetalhes.Select(y => y.fk_empresa).Distinct();
+
+                foreach (var it in lstEmps)
+                {
+                    var e = db.T_Empresa.FirstOrDefault(y => y.i_unique == it).st_empresa;
+
+                    item.empresas += e + ", ";
+                }
+
+                item.empresas = item.empresas.Trim().TrimEnd(',');
+            }
 
             return Ok(new
             {

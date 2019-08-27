76167380720260c569e5a04a35e9c7ee13da64a7
rodrigo groff <rodrigo.groff@gmail.com>
2019-08-26 23:22:55 -0300

    ajustes

---
+++ ClientISOv2/ClientISO.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ClientISOv2
+{
+    public class ClientISO
+    {
+        byte[] bytes = new byte[1024];
+        Socket senderSock;
+
+        public void Start()
+        {
+            try
+            {
+                // Create one SocketPermission for socket access restrictions 
+                SocketPermission permission = new SocketPermission(
+                    NetworkAccess.Connect,    // Connection permission 
+                    TransportType.Tcp,        // Defines transport types 
+                    "",                       // Gets the IP addresses 
+                    SocketPermission.AllPorts // All ports 
+                    );
+
+                // Ensures the code to have permission to access a Socket 
+                permission.Demand();
+
+                // Resolves a host name to an IPHostEntry instance            
+                IPHostEntry ipHost = Dns.GetHostEntry("");
+
+                // Gets first IP address associated with a localhost 
+                IPAddress ipAddr = ipHost.AddressList[1];
+
+                // Creates a network endpoint 
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 4510);
+
+                // Create one Socket object to setup Tcp connection 
+                senderSock = new Socket(
+                    ipAddr.AddressFamily,// Specifies the addressing scheme 
+                    SocketType.Stream,   // The type of socket  
+                    ProtocolType.Tcp     // Specifies the protocols  
+                    );
+
+                senderSock.NoDelay = false;   // Using the Nagle algorithm 
+
+                // Establishes a connection to a remote host 
+                senderSock.Connect(ipEndPoint);
+
+                Console.WriteLine ( "Socket connected to " + senderSock.RemoteEndPoint.ToString() );
+
+                while (true) { if (DateTime.Now.Second == 1) break; Thread.Sleep(10); }
+
+                for (int i = 0; i < 1; i++)
+                {
+                    var x = Guid.NewGuid().ToString();
+
+                    SendToServer("200 " + i + " " + x);
+
+                    var y = ReceiveDataFromServer();
+
+                    Console.WriteLine("Rec from server: " + y);
+
+                    Thread.Sleep(100);
+                }
+            }
+            catch (Exception exc) {
+                Console.WriteLine(exc.ToString());
+            }
+        }
+
+        public void SendToServer(string tbMsgToSend)
+        {
+            //Console.WriteLine("Send..." + tbMsgToSend);
+
+            try
+            {
+                byte[] byteData = Encoding.ASCII.GetBytes(tbMsgToSend);
+                senderSock.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), senderSock);
+            }
+            catch (SocketException exc)
+            {
+                //Console.WriteLine("Não conseguiu enviar!");
+            }
+            catch (Exception exc)
+            {
+                //Console.WriteLine("*** Send + " + exc.ToString());
+            }
+        }
+
+        public void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket handler = (Socket)ar.AsyncState;
+                int bytesSend = handler.EndSend(ar);
+
+                
+            }
+            catch (SocketException exc)
+            {
+                //Console.WriteLine("Não conseguiu enviar!");
+            }
+            catch (Exception exc)
+            {
+                //Console.WriteLine("*** SendCallback + " + exc.ToString());
+            }
+        }
+
+        public void SendSincrono(string theMessageToSend)
+        {
+            Console.WriteLine("Send >> " + theMessageToSend);
+            try
+            {
+                byte[] msg = Encoding.ASCII.GetBytes(theMessageToSend);
+                
+                // Sends data to a connected Socket. 
+                int bytesSend = senderSock.Send(msg);
+
+                ReceiveDataFromServer();                
+            }
+            catch (Exception exc) {
+                Console.WriteLine(exc.ToString());
+            }
+        }
+
+        private string ReceiveDataFromServer()
+        {
+            try
+            {
+                // Receives data from a bound Socket. 
+                int bytesRec = senderSock.Receive(bytes);
+
+                // Converts byte array to string 
+                String theMessageToReceive = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                // Continues to read the data till data isn't available 
+                while (senderSock.Available > 0)
+                {
+                    bytesRec = senderSock.Receive(bytes);
+                    theMessageToReceive += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                }
+
+                Console.WriteLine("Rec >> " + theMessageToReceive);
+
+                return theMessageToReceive;
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.ToString());
+
+                return null;                
+            }
+        }
+
+        private void Disconnect()
+        {
+            try
+            {
+                senderSock.Shutdown(SocketShutdown.Both);
+                senderSock.Close();
+
+                Console.WriteLine("Saiu!");
+            }
+            catch (Exception exc) {
+                Console.WriteLine(exc.ToString());
+            }
+        }
+    }
+}
---
+++ ServerIsoV2/ServerISO.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ServerIsoV2
+{
+    public class Command
+    {
+        public string Text { get; set; }
+
+        public Socket handler { get; set; }
+    }
+    
+    public class ServerISO
+    {
+        public List<Command> Commands = new List<Command>();
+                
+        public void Start()
+        {            
+            var permission = new SocketPermission( NetworkAccess.Accept, TransportType.Tcp, "", SocketPermission.AllPorts );             
+            permission.Demand();
+            
+            IPHostEntry ipHost = Dns.GetHostEntry("");            
+            IPAddress ipAddr = ipHost.AddressList[1];
+            
+            var ipEndPoint = new IPEndPoint(ipAddr, 4510);            
+            var sListener = new Socket( ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp );
+            sListener.Bind(ipEndPoint);
+            sListener.Listen(1000);
+            
+            AsyncCallback aCallback = new AsyncCallback(AcceptCallback);
+            sListener.BeginAccept(aCallback, sListener);
+
+            Console.WriteLine( "Server is now listening on " + ipEndPoint.Address + " port: " + ipEndPoint.Port );
+
+            while (true)
+            {
+                if (Commands.Count > 0 )
+                {
+                    Console.WriteLine("Queue: " + Commands.Count);
+
+                    var cmd = Commands[0];
+                    Commands.RemoveAt(0);
+
+                    Thread thread = new Thread(() => ProcessCommand(cmd));
+                    thread.Start();
+                }
+
+                Thread.Sleep(100);
+            }                
+        }
+
+        public void ProcessCommand (Command cmd)
+        {
+            Console.WriteLine("CMD> " + cmd.Text);
+            Send("210 " + cmd.Text, cmd.handler);
+        }
+
+        public byte[] GetBuffer()
+        {
+            return new byte[999999];
+        }
+
+        public void AcceptCallback(IAsyncResult ar)
+        {
+            try
+            {
+                byte[] buffer = GetBuffer();
+                
+                var listener = (Socket)ar.AsyncState;
+                var handler = listener.EndAccept(ar);
+
+                handler.NoDelay = false;
+
+                object[] obj = new object[2];
+                obj[0] = buffer;
+                obj[1] = handler;
+
+                handler.BeginReceive( buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), obj );
+                listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("*** AcceptCallback + " + exc.ToString());
+            }
+        }
+
+        public void ReceiveCallback(IAsyncResult ar)
+        {
+            var obj = ar.AsyncState as object[];
+            byte[] buffer = obj[0] as byte[];
+            var handler = obj[1] as Socket;
+
+            string content = string.Empty;
+
+            try
+            {
+                int bytesRead = handler.EndReceive(ar);
+
+                if (bytesRead > 0)
+                {
+                    content += Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine("RECV: " + content);
+
+                    Commands.Add(new Command
+                    {
+                        Text = content,
+                        handler = handler
+                    });                    
+                }      
+                else
+                {
+                    Listen(handler);
+                }
+            }
+            catch (SocketException exc)
+            {
+                Console.WriteLine(exc);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc);
+            }
+        }
+
+        public void Listen(Socket handler)
+        {
+            Console.WriteLine("Listen....");
+
+            object[] obj = new object[2];
+            byte[] buffernew = GetBuffer();
+            obj[0] = buffernew;
+            obj[1] = handler;
+
+            handler.BeginReceive(buffernew, 0, buffernew.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), obj);
+        }
+
+        public void Send(string tbMsgToSend, Socket handler)
+        {
+            try
+            {
+                var obj = new object[2];
+                byte[] byteData = Encoding.ASCII.GetBytes(tbMsgToSend);
+                obj[0] = byteData;
+                obj[1] = handler;
+
+                handler.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), obj);                
+            }
+            catch (SocketException exc)
+            {
+                Console.WriteLine(exc);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc);
+            }
+        }
+
+        public void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                var obj = ar.AsyncState as object[];
+                var handler = obj[1] as Socket;
+                int bytesSend = handler.EndSend(ar);
+
+                Listen(handler);
+            }
+            catch (SocketException exc)
+            {
+                Console.WriteLine(exc);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc);
+            }
+        }        
+    }
+}

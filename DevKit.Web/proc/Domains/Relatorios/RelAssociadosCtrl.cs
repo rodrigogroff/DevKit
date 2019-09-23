9a94914795281468981f5d98658c877705afea10
rodrigo groff <rodrigo.groff@vortigo.com.br>
2019-09-23 18:14:06 -0300

    ajustes

--- DevKit.Web/proc/Domains/Relatorios/RelAssociadosCtrl.cs
+++ DevKit.Web/proc/Domains/Relatorios/RelAssociadosCtrl.cs
@@ -129,7 +129,7 @@ namespace DevKit.Web.Controllers
                          select e);
             }
 
-            if (stEmpresa != "")
+            if (!string.IsNullOrEmpty(stEmpresa))
             {
                 query = (from e in query
                          where e.st_empresa == stEmpresa

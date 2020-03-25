98e072c99b8e1dddc5d2c5a96fdefe66c709b226
rodrigo groff <rodrigo.groff@gmail.com>
2020-03-25 11:44:27 -0300

    ajustesw

--- PortalSite/src/shared/Api.js
+++ PortalSite/src/shared/Api.js
@@ -1,14 +1,14 @@
 ﻿
 export const ApiLocation = {
-    api_host: 'http://localhost',
-    //api_host: 'https://meuconvey.conveynet.com.br',
+    //api_host: 'http://localhost',
+    api_host: 'https://meuconvey.conveynet.com.br',
     api_port: '18524',
     api_portal: '/api/v1/portal/',
 }
 
 export class Api {
 
-    versao = () => "v2.1.012";
+    versao = () => "v2.1.014";
 
     isAuthenticated = () => localStorage.getItem('token');
 

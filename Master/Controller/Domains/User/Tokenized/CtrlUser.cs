fbaead0143da6c15e5f74c7e3aa73745e6d2fc46
rodrigo groff <rodrigo.groff@gmail.com>
2021-03-09 15:34:52 -0300

    ajustes

--- Master/Controller/Domains/User/Tokenized/CtrlUser.cs
+++ Master/Controller/Domains/User/Tokenized/CtrlUser.cs
@@ -1,5 +1,4 @@
-﻿using Entities.Api.User;
-using Master;
+﻿using Master;
 using Master.Infra.Entity.Database;
 using Master.Repository;
 using Master.Service;
@@ -12,22 +11,6 @@ namespace Api.Master.Controllers
     public partial class CtrlUserListing : MasterController
     {
         public CtrlUserListing(IOptions<LocalNetwork> _network, IMemoryCache _cache) : base(_network, _cache) { }
-            
-        [HttpGet]
-        [Route("api/users")]
-        public ActionResult<DtoUserList> users(string search)
-        {
-            var au = GetCurrentAuthenticatedUser();
-
-            var repo = new DapperUserRepository();
-            var srv = new SrvUserListV1(repo);
-            var dto = new DtoUserList();
-
-            if (!srv.Exec(network, au, search, ref dto))
-                return BadRequest(srv.Error);
-
-            return Ok(dto);
-        }
 
         [HttpGet]
         [Route("api/user")]
--- Master/Repository/Database/Country.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Master.Infra.Entity.Database
-{
-    public partial class Country
-    {
-        public long id { get; set; }
-        public string stName { get; set; }
-    }
-}
